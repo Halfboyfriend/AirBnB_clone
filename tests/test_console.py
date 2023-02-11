8bbe9e18d45955c358d631bdad44e84e6763d489
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-02-11 14:15:24 +0100

    test

---
+++ tests/test_console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Unit tests for console using MM from python standard library
+Checks console for capturing stdout into a StringIO object
+"""
+
+import os
+import sys
+import unittest
+
+
+
+class TestConsole(unittest.TestCase):
+    """
+    Unittest for the console model
+    """
+
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
