4aeca3bd173d292b2993a687ea3241c0559ea605
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-02-11 15:20:58 +0100

    base_models

---
+++ models/base_model.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""
+Custom base class for all project work.
+"""
+
+import uuid
+from datetime import datetime
+import models
+
+class BaseModel():
+    """
+    BASEMODEL CLASS
+
+     Attributes:
+        id(str): handles unique user identity
+        created_at: assigns current datetime
+        updated_at: updates current datetime
+    Methods:
+        __str__: prints the class name, id, and creates dictionary
+        representations of the input values
+        save(self): updates instance attributes with current datetime
+        to_dict(self): returns the dictionary values of the instance obj
+    """
+
+    def __init__(self, *args, **kwargs):
+
+        DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+
+        else:
+            for key, value in kwargs:
+                if key in ("updated_at", "created_at"):
+                    self.__dict__[key] = datetime.strptime(value, DATE_FORMAT)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self.__dict__[key] = value
+
+    def __str__(self):
+        """
+        return: Strings rep of class
+        """
+        return f"[{self.__class__.__name__} ({self.id}) {self.__dict__}]"
+
+    def save(self):
+        """
+        return: update the public instance 'updated_time' with the current date
+        time.
+        """
+        self.updated_at = datetime.utcnow()
+
+
+
+    def to_dict(self):
+        """
+        return: a dictionary containing all key/value pairs
+        """
+        all_dict = {}
+
+        for k,v in self.__dict__.items():
+            if k == "created_at" or k == "updated_at":
+                all_dict[k] = v
+
+            else:
+                all_dict[k] = v
+        return all_dict
+
+
+
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,35 @@
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Tests attributes of the base model
+    """
+
+    def setUp(self):
+        """
+        Classes needed for testing
+        """
+        pass
+
+    def test_basic(self):
+        """
+        Tests basic imputs for the BaseModel class
+        """
+        my_model = BaseModel()
+        my_model.name = "ALX_HBNB"
+        my_model.number = 89
+        self.assertEqual([my_model.name, my_model.number],
+                         ["ALX_HBNB", 89])
+
+    def test_datetime(self):
+        """
+        Tests for correct datetime format
+        """
+        pass
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
