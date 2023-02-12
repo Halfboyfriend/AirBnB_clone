e9b304867543164024e9024a8837a4de8242e756
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-02-12 10:39:11 +0100

    tests

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,6 @@
-from engine.file_storage import FileStorage
+"""__init__ magic method for models directory"""
+from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
